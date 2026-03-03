b79d66b0f4a9e7167fc7ab2366d1f09cc709c526
ap8938 <ap8938@srmist.edu.in>
2026-03-03 22:21:36 +0530

    Added UC6

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,61 +1,75 @@
+/*public class OopsBannerApp
+{
+    public static void main(String[] args)
+    {
+        String[] lines = new String[7];
+
+        lines[0]=String.join(" ","   ***  "  , "   ***  " ,  " *******  " , "  *****");
+        lines[1]=String.join(" ","  ** ** "  , "  ** ** " ,  " **     **" , " **");
+        lines[2]=String.join(" "," **   **"  , " **   **" ,  " **     **" , " **");
+        lines[3]=String.join(" "," **   **"  , " **   **" ,  " *******  " , " *****");
+        lines[4]=String.join(" "," **   **"  , " **   **" ,  " **       " , "      **");
+        lines[5]=String.join(" ","  ** ** "  , "  ** ** " ,  " **       " , "      **");
+        lines[6]=String.join(" ","   ***  "  , "   ***  " ,  " **       " , "  *****");
+
+        for (String line:lines)
+        {
+            System.out.println(line);
+        }
+    }
+}*/
 
 public class OopsBannerApp {
 
-    public static void main(String[] args) {
-
-        String[] lines = {
-            String.join("   ",
-                "  *****  ",   
-                "  *****  ",   
-                " ******  ",   
-                "  *****  "    
-            ),
-
-            String.join("   ",
-                " **   ** ",   
-                " **   ** ",   
-                " **   ** ",   
-                " **      "    
-            ),
-
-            String.join("   ",
-                " **   ** ",   
-                " **   ** ",   
-                " ******  ",   
-                "  *****  "    
-            ),
-
-            String.join("   ",
-                " **   ** ",   
-                " **   ** ",   
-                " **      ",   
-                "       ** "   
-            ),
-
-            String.join("   ",
-                " **   ** ",   
-                " **   ** ",   
-                " **      ",   
-                " **   ** "    
-            ),
-
-            String.join("   ",
-                "  *****  ",   // O
-                "  *****  ",   // O
-                " **      ",   // P
-                "  *****  "    // S
-            ),
-
-            String.join("   ",
-                "          ",
-                "          ",
-                "          ",
-                "          "
-            )
+    public static String[] getOPattern() {
+        return new String[]{
+            "  *****  ",
+            " **   ** ",
+            " **   ** ",
+            " **   ** ",
+            " **   ** ",
+            "  *****  ",
+            "         "
         };
+    }
 
-        // Enhanced for-each loop
-        for (String line : lines) {
+    public static String[] getPPattern() {
+        return new String[]{
+            " ******  ",
+            " **   ** ",
+            " ******  ",
+            " **      ",
+            " **      ",
+            " **      ",
+            "         "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+            "  *****  ",
+            " **      ",
+            "  *****  ",
+            "       **",
+            " **   ** ",
+            "  *****  ",
+            "         "
+        };
+    }
+
+    public static void main(String[] args) {
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();
+        String[] p  = getPPattern();
+        String[] s  = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", o1[i], o2[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
             System.out.println(line);
         }
     }
