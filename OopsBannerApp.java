c3529230f69bd2dfb8ac3b3ca135395859fd5abc
ap8938 <ap8938@srmist.edu.in>
2026-03-03 22:03:52 +0530

    Added UC5

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,23 +1,3 @@
-/*public class OopsBannerApp
-{
-    public static void main(String[] args)
-    {
-        String[] lines = new String[7];
-
-        lines[0]=String.join(" ","   ***  "  , "   ***  " ,  " *******  " , "  *****");
-        lines[1]=String.join(" ","  ** ** "  , "  ** ** " ,  " **     **" , " **");
-        lines[2]=String.join(" "," **   **"  , " **   **" ,  " **     **" , " **");
-        lines[3]=String.join(" "," **   **"  , " **   **" ,  " *******  " , " *****");
-        lines[4]=String.join(" "," **   **"  , " **   **" ,  " **       " , "      **");
-        lines[5]=String.join(" ","  ** ** "  , "  ** ** " ,  " **       " , "      **");
-        lines[6]=String.join(" ","   ***  "  , "   ***  " ,  " **       " , "  *****");
-
-        for (String line:lines)
-        {
-            System.out.println(line);
-        }
-    }
-}*/
 
 public class OopsBannerApp {
 
@@ -25,38 +5,38 @@ public class OopsBannerApp {
 
         String[] lines = {
             String.join("   ",
-                "  *****  ",   // O
-                "  *****  ",   // O
-                " ******  ",   // P
-                "  *****  "    // S
+                "  *****  ",   
+                "  *****  ",   
+                " ******  ",   
+                "  *****  "    
             ),
 
             String.join("   ",
-                " **   ** ",   // O
-                " **   ** ",   // O
-                " **   ** ",   // P
-                " **      "    // S
+                " **   ** ",   
+                " **   ** ",   
+                " **   ** ",   
+                " **      "    
             ),
 
             String.join("   ",
-                " **   ** ",   // O
-                " **   ** ",   // O
-                " ******  ",   // P
-                "  *****  "    // S
+                " **   ** ",   
+                " **   ** ",   
+                " ******  ",   
+                "  *****  "    
             ),
 
             String.join("   ",
-                " **   ** ",   // O
-                " **   ** ",   // O
-                " **      ",   // P
-                "       ** "   // S
+                " **   ** ",   
+                " **   ** ",   
+                " **      ",   
+                "       ** "   
             ),
 
             String.join("   ",
-                " **   ** ",   // O
-                " **   ** ",   // O
-                " **      ",   // P
-                " **   ** "    // S
+                " **   ** ",   
+                " **   ** ",   
+                " **      ",   
+                " **   ** "    
             ),
 
             String.join("   ",
